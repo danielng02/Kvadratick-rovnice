4223b154b6ab89b9c61f4b3f59ea38803903a2a6
Nguyen Manh Hung <nguyen.2018@ssps.cz>
2021-06-14 09:38:45 +0200

    Přidejte soubory projektu.

---
+++ Kvadratická rovnice/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KvadratickaRovnice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            Koren koren = new Koren(Koren.Equation(a, b, c));
+
+            double x1 = koren.x1;
+            double x2 = koren.x2;
+
+            Console.WriteLine(x1 + "\n" + x2);
+
+            Console.ReadKey();
+        }
+    }
+    class Koren
+    {
+        public double x1;
+        public double x2;
+        public static Koren Equation(double a, double b, double c)
+        {
+            Koren koren = new Koren();
+
+            double diskriminant = Math.Sqrt(Math.Pow(b, 2) - (4 * a * c));
+
+            if (diskriminant == 0)
+            {
+                koren.x1 = koren.x2 = -b / (2 * a);
+            }
+
+            if (diskriminant > 0)
+            {
+                koren.x1 = (b * (-1) + diskriminant) / (2 * a);
+                koren.x2 = (b * (-1) - diskriminant) / (2 * a);
+            }
+
+            return koren;
+
+        }
+
+        public Koren() { }
+
+        public Koren(Koren koren)
+        {
+            x1 = koren.x1;
+            x2 = koren.x2;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KvadratickaRovnice;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+
+        [DataRow(2, -11, 14, 2, 7/2)]
+        public void TestMethod1(double a, double b, double c)
+        {
+            //Arrange
+
+            //Act
+            
+            //Assert
+        }
+    }
+}
