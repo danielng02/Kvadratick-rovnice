9ffd3ce75d15f76d9600ad84d11d8e8fe6f603b5
Dan nguyen <danek.nguyen@gmail.com>
2021-06-14 10:13:19 +0200

    prvni test

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -8,14 +8,17 @@ namespace UnitTestProject1
     {
         [TestMethod]
 
-        [DataRow(2, -11, 14, 2, 7/2)]
-        public void TestMethod1(double a, double b, double c)
+        [DataRow(2, -11, 14, 2, 3.5)]
+        public void TestMethod1(double a, double b, double c, double x1, double x2)
         {
             //Arrange
 
             //Act
-            
+            Koren koren = new Koren(Koren.Equation(a,b,c));
+
             //Assert
+            Assert.AreEqual(x2, koren.x1);
+            Assert.AreEqual(x1, koren.x2);
         }
     }
 }
